78de9d5d31e3c600eb537138b99ab71292357bbd
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-08 17:54:24 +0200

    admin cats

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 $(function(){
     /** Cat constructor */
-    function Cat(name) {
+    function Cat(name, pic, clicks) {
         this.name = name;
-        this.pic = name + '.jpg';
-        this.clicks = 0;
+        this.pic = pic;
+        this.clicks = clicks || 0;
+    }
+    Cat.prototype.update = function(name, pic, clicks) {
+        this.name = name;
+        this.pic = pic;
+        this.clicks = clicks || 0;
     }
 
     /** model */
     var model = {
         cats: [
-            new Cat('missy'), 
-            new Cat('garfield'), 
-            new Cat('isir'), 
-            new Cat('chuck'), 
-            new Cat('rya')
+            new Cat('missy', 'http://cosmouk.cdnds.net/15/42/1445004076-white-cat-bag.jpg'), 
+            new Cat('garfield', 'http://www.catster.com/wp-content/uploads/2015/06/can-cats-eat-peanut-butter-02.jpg'), 
+            new Cat('isir', 'http://www.catster.com/wp-content/uploads/2015/06/albino-cats-kittens-07.jpg'), 
+            new Cat('chuck', 'http://s5.favim.com/orig/51/animals-cats-cute-feline-photography-Favim.com-551634.jpg'), 
+            new Cat('rya', 'http://stuffpoint.com/cats/image/87009-cats-cute-cat.jpg')
         ],
-        selectedCat: null
+        selectedCat: null,
+        isAdminShown: false
         
     }
 
@@ -24,6 +30,7 @@ $(function(){
         init: function() {
             catsView.init();
             catView.init();
+            adminView.init();
         },
         getCats: function() {
             return model.cats;
@@ -34,31 +41,57 @@ $(function(){
         setCurrentCat: function(cat) {
             model.selectedCat = cat;
         },
+        setCurrentCatView: function(cat, updateList) {
+            catView.render(cat);
+            adminView.render(cat);
+            this._updateClicksView();
+            if(updateList) {
+                catsView.render();
+            }
+        },
         changeCat: function(cat) {
             this.setCurrentCat(cat);
-            catView.render(cat);
-            this._updateClicksView();
+            this.setCurrentCatView(cat);
         },
         countClicks: function() {
             this.getCurrentCat().clicks++; 
             this._updateClicksView();
         },
         _updateClicksView: function() {
-            catView.updateClicks(this.getCurrentCat().clicks);
+            var clicks = this.getCurrentCat().clicks;
+            catView.updateClicks(clicks);
+            adminView.updateClicks(clicks);
+        },
+        getAdminState: function() {
+            return model.isAdminShown;
+        },
+        setAdminState: function(state) {
+            model.isAdminShown = state;
+        },
+        changeDataCat: function(data) {
+            var indexCat = $.inArray(model.selectedCat, model.cats); 
+            var updateList = data.pic !== model.selectedCat.pic || 
+                data.name !== model.selectedCat.name;
+            if(indexCat > -1) {
+                model.cats[indexCat].update(data.name, data.pic, parseInt(data.clicks)); 
+                this.setCurrentCatView(this.getCurrentCat(), updateList);           
+            }
         }
     };
 
     /** cats list view */
     var catsView = {
         init: function() {
-            var thumbsDir = 'cats/thumbs/',
-            divList = $('#catsList > ul'),
-            elements = [];
-
+            this.divList = $('#catsList > ul');
+            this.render();
+        },
+        render: function() {
+            var elements = [];
+            this.divList.empty();
             octopus.getCats().forEach(function(cat) {
                 var li = $('<li/>'),
                 img = $('<img/>', {
-                    src: thumbsDir + cat.pic,
+                    src: cat.pic,
                     alt: cat.name
                 });
 
@@ -72,7 +105,7 @@ $(function(){
                 })(cat));
             });
 
-            divList.append(elements);
+            this.divList.append(elements);
         }
     };
 
@@ -84,13 +117,12 @@ $(function(){
         },
         render: function(cat) {
             var catDiv = this.catDiv,
-            imagesDir = 'cats/images/',
             name = $('<h2>' + cat.name + '</h2>'),
             img = $('<img/>', {
-                src: imagesDir + cat.pic,
+                src: cat.pic,
                 alt: cat.name
             });
-            catDiv.html('').append([name, img]);
+            catDiv.empty().append([name, img]);
 
             img.on('click', function() {
                 octopus.countClicks();
@@ -99,7 +131,46 @@ $(function(){
         updateClicks: function(clicks) {
             this.clicksEl.html(clicks);
         }
-    }
+    };
+
+    /** admin view */
+    var adminView = {
+        init: function() {
+            this.nameInput = $('#nameField');
+            this.picInput = $('#picField');
+            this.clicksInput = $('#clicksField');
+
+            $('.showHideAdmin').on('click', function() {
+                var state = octopus.getAdminState();
+                $('#admin').toggleClass('hide', state);
+                octopus.setAdminState(!state);
+            });
+            $('#saveAdmin').on('click', $.proxy(function() {
+                var data = {
+                    name: this.nameInput.val(),
+                    pic: this.picInput.val(),
+                    clicks: this.clicksInput.val()
+                };
+                octopus.changeDataCat(data);
+            }, this));
+        },
+        render: function(cat) {
+            this.nameInput.val(cat.name);
+            this.picInput.val(cat.pic);
+            this.clicksInput.val(cat.clicks);
+        },
+        updateClicks: function(clicks) {
+            this.clicksInput.val(clicks);
+        },
+        saveCatInfo: function() {
+            var data = {
+                name: this.nameInput.val(),
+                pic: this.picInput.val(),
+                clicks: (this.clicksInput.val())
+            };
+            octopus.changeDataCat(data);
+        }
+    };
     octopus.init();
 
 }());
